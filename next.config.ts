95aef300a15011b0b67f0509211d989c4a1fd1fb
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2026-07-19 12:46:02 +0600

    Configure base path and asset prefix in next.config.ts
    Set base path and asset prefix for production environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  // Set the base path to your repository name, including a leading slash
+  basePath: process.env.NODE_ENV === "production" ? "/ztalab0" : undefined,
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/ztalab0" : undefined, // Ensures assets like images/CSS load correctly
   images: {
     unoptimized: true,
   },
